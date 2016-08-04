d2dc7b667027cac8bf8b40cde1ffdaa3b14a5148
joe <carrborojoe@gmail.com>
2016-08-04 12:45:04 -1000

    renamed theme and child theme

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 			<div class='col-xs-12 col-sm-4'>
 				<dl>
 					<dt>WordPress Links</dt>
-					<dd><a href='' target='_blank'>WordPress Links</a></dd>
+					<dd><a href='http://www.wordpress.org' target='_blank'>WordPress.org</a></dd>
 					<dd><a href='' target='_blank'>WordPress Links</a></dd>
 					<dd><a href='' target='_blank'>WordPress Links</a></dd>
 					<dd><a href='' target='_blank'>WordPress Links</a></dd>
---
+++ page-about.php
@@ -0,0 +1,28 @@
+<!-- This TEMPLATE page_about - page name starts with page_  which is a dedicated wp  prefix before the slug name / substitute for index.php.  WordPress looks for page_ templates before other php files.-->
+
+
+<!-- looks for a file called header.php -->
+<?php get_header(); ?>
+
+	<div id="content">
+		
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>	
+			<h2><?php the_title(); ?></h2>
+			
+			<!--<h4>Posted on <?php the_time('F jS, Y') ?></h4>-->
+			
+			<p><?php the_content(__('(more...)')); ?></p>
+			
+			<hr> 
+		<?php endwhile; else: ?>
+		
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+
+		<?php endif; ?>
+	</div>
+
+
+		<?php //the_content();  ?>
+
+<?php get_footer(); ?>
+
